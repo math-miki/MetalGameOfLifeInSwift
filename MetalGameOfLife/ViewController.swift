ec6113c9d40211a02ed95e29b89f7a772070e1ba
Miki Takahashi <math.micky.dtl@gmail.com>
2018-11-09 02:15:08 +0900

    vc

--- MetalGameOfLife/ViewController.swift
+++ MetalGameOfLife/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
-
+    var renderer: GOLRenderer!
+    var metalView: MTKView!
+    var metalDevice: MTLDevice!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.metalView = MTKView()
+        
+        self.view = metalView
+        
+        self.setupView()
+    }
+    
+    // MARK: - Setup Methods
+    func setupView() {
+        self.metalView.device = MTLCreateSystemDefaultDevice()
+        self.metalView.colorPixelFormat = MTLPixelFormat.rgba8Unorm
+        self.metalView.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        self.metalView.drawableSize = self.metalView.bounds.size
+        
+        self.renderer = GOLRenderer(withView: metalView)
     }
 
-
+    // MARK: - Interaction (Touch) Handling
+    func locationInGridForLocationInView(point: CGPoint) -> CGPoint {
+        let viewSize = self.view.frame.size
+        let normalizedWidth = point.x/viewSize.width
+        let normalizedHeight = point.y/viewSize.height
+        let gridSize = self.renderer.gridSize as! MTLSize
+        let gridX = Int(round(normalizedWidth * CGFloat(gridSize.width)))
+        let gridY = Int(round(normalizedHeight * CGFloat(gridSize.height)))
+        return CGPoint(x: gridX, y: gridY)
+    }
+    
+    func activateRandomCellsForPoint(point: CGPoint) {
+        let gridLocation = self.locationInGridForLocationInView(point: point)
+        self.renderer.activateRandomCellsInNeighborhoodOfCell(cell: gridLocation)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self.view)
+            self.activateRandomCellsForPoint(point: location)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self.view)
+            self.activateRandomCellsForPoint(point: location)
+        }
+    }
+    
 }
 
