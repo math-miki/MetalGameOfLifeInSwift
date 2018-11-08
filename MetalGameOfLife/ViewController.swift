78784a3fd9364203915dd7d4e17d529e83bf2201
Miki Takahashi <math.micky.dtl@gmail.com>
2018-11-08 19:08:33 +0900

    Initial Commit

---
+++ MetalGameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalGameOfLife
+//
+//  Created by MikiTakahashi on 2018/11/08.
+//  Copyright © 2018 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
